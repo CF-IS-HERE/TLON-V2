8f622d9a18f12a654abb5e7d4118940c3ca7ad5f
Nicolas Bize <nicolasbize@gmail.com>
2022-05-27 17:28:56 -0700

    fixed the splat color not associated with the lemon color

--- src/assemblies/lemon.lua
+++ src/assemblies/lemon.lua
@@ -35,7 +35,7 @@ return function(entity, options)
     local filename = "lemon"..nb..".png"
     local x, y = getRandomPosition()
     entity:give("sprite", {
-        image = images[math.random(1, 3)],
+        image = images[nb],
     })
     entity:give("layer", options.canvas)
     entity:give("ai_controlled")
@@ -82,7 +82,7 @@ return function(entity, options)
                 AudioWorld:emit("playEnemyHitSound")
             else
                 lemon.particles.emitters.explode.ticks = 1
-                lemon.particles.emitters.explode.offset = lemon.knockback
+                lemon.particles.emitters.explode.offset = {x = lemon.knockback.x * -1, y = lemon.knockback.y * -1}
                 lemon.particles.emitters.explode.rotation = {a=knockback_angle, b=knockback_angle}
                 -- only destroy the entity after the splat is destroyed
                 lemon.active = false
