ecaf56863f62bd94108c387340588c94637a6e30
Nicolas Bize <nicolasbize@gmail.com>
2022-05-30 19:03:43 -0700

    updated player to rotate around when moving

--- src/systems/PlayerControlSystem.lua
+++ src/systems/PlayerControlSystem.lua
@@ -2,9 +2,12 @@ local PlayerControlSystem = Concord.system({
     pool = {"player_controlled", "speed", "position", "velocity", "sprite", "particles", "active"}
 })
 
+local timer = 0
+
 function PlayerControlSystem:update(dt)
+    timer = timer + dt
     for _, entity in ipairs(self.pool) do
-        if entity.active then 
+        if entity.active then
             local left = love.keyboard.isDown("a") and 1 or 0
             local right = love.keyboard.isDown("d") and 1 or 0
             local up = love.keyboard.isDown("w") and 1 or 0
@@ -16,6 +19,7 @@ function PlayerControlSystem:update(dt)
             entity.sprite.flipped = love.mouse.getX() / 4 < entity.position.x + entity.sprite.offset.x
             local is_moving = (math.abs(entity.velocity.x) + math.abs(entity.velocity.y)) / entity.speed.value
             entity.particles.emitters.run.ticks = MathUtils.round(is_moving)
+            entity.sprite.rotation = math.sin(timer * 12) * 0.2 * is_moving
         end
     end
 end
